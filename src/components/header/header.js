02e2e41a190fe274e4ba504bebe7a3bc3f418268
garrett <yien.garrett94@gmail.com>
2021-07-14 17:37:30 -0500

    attempted stretch but got stuck passing down a callback

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,8 +3,10 @@ import "./Header.css";
 import Logo from './logo';
 import Title from './title';
 import Nav from './nav';
+import Datesearch from './datesearch';
 
 const Header = (props) => {
+    const { findDate } = props;
     return (
         <header className="header-container">
             <div className="upper-container">
@@ -18,6 +20,9 @@ const Header = (props) => {
             <div className="lower-container">
                 <div className="nav-bar">
                     <Nav />
+                </div>
+                <div className="date-query">
+                    <Datesearch findDate={() => findDate()}/>
                 </div>
             </div>
         </header>
