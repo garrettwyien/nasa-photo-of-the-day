54b868baf1f2150d15223bff0a3fb1d863888ebc
garrett <yien.garrett94@gmail.com>
2021-07-14 16:25:41 -0500

    created basic jsx for header and content sections

---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Header.css";
+import Logo from './logo';
+import Title from './title';
+import Nav from './nav';
+
+const Header = (props) => {
+    return (
+        <div className="header-container">
+            <div className="upper-container">
+                <div className="logo">
+                    <Logo />
+                </div>
+                <div className="title">
+                    <Title />
+                </div>
+            </div>
+            <div className="lower-container">
+                <div className="nav-bar">
+                    <Nav />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
